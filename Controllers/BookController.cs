c63ddd562328c900af05f793c8a6d23fa04f9496
carelau <carelau2812@gmail.com>
2021-10-01 00:44:36 -0400

    implement funtionality to add search and delete

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -11,47 +11,104 @@ using Bookish.Services;
 
 namespace Bookish.Controllers
 {
-    [Route("books")]
+
     public class BookController : Controller
     {
         private readonly ILogger<BookController> _logger;
         private readonly BookService _bookService;
-        
-    
-        public BookController (ILogger<BookController> logger, BookService bookService)
+
+
+        public BookController(ILogger<BookController> logger, BookService bookService)
         {
             _logger = logger;
             _bookService = bookService;
         }
 
-        public IActionResult BookView()
+        public IActionResult Library()
         {
             return View(_bookService.GetBooks());
         }
 
+        public IActionResult SearchBook(bool isSuccess = false)
+        {
+            ViewBag.IsSuccess = isSuccess;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SingleBook(Book book)
+        {
+            Book searchedbook = _bookService.GetASingleBook(book.Id);
+            if (searchedbook == null)
+            {
+                return RedirectToAction("SearchBook", new { isSuccess = true });
+            }
+            return View(searchedbook);
+        }
+
+        public IActionResult AddNewBook(bool isSuccess = false)
+        {
+            ViewBag.IsSuccess = isSuccess;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddBookToDatabase(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                int bookId = _bookService.CreateBook(book);
+
+                if (bookId > 0)
+                {
+                    return RedirectToAction("AddNewBook", new { isSuccess = true });
+                }
+            }
+            return View();
+        }
+
+        public IActionResult DeleteBook(bool isSuccess = false)
+        {
+            ViewBag.IsSuccess = isSuccess;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DeleteBookInDatabase(Book book)
+
+        {
+            Book searchedbook = _bookService.GetASingleBook(book.Id);
+            if (searchedbook == null)
+            {
+                return RedirectToAction("DeleteBook", new { isSuccess = true });
+            }
+            _bookService.DeleteBook(book);
+            return View();
+
         }
     }
+}
 
-        /*  public IActionResult Index()
-          {
+/*  public IActionResult Index()
+  {
 
-                List<Book> myBookList = new List<Book>()
-               {
-                  new Book {Id= 1, Title = "Klara and the Sun", Author = "Kazuo Ishiguro"},
-                  new Book {Id= 2, Title = "Effortless", Author = "Greg McKeown"},
-                  new Book {Id= 3, Title = "Gold Diggers", Author = "Sanjena Sathian"},
-               };
+        List<Book> myBookList = new List<Book>()
+       {
+          new Book {Id= 1, Title = "Klara and the Sun", Author = "Kazuo Ishiguro"},
+          new Book {Id= 2, Title = "Effortless", Author = "Greg McKeown"},
+          new Book {Id= 3, Title = "Gold Diggers", Author = "Sanjena Sathian"},
+       };
 
-              return View(myBookList);
-          }
-          */
+      return View(myBookList);
+  }
+  */
 
 
-        // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        // public IActionResult Error()
-        // {
-        //     return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        // }
+// [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+// public IActionResult Error()
+// {
+//     return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+// }
 /*
         private void AddBook()
         {
@@ -71,4 +128,4 @@ namespace Bookish.Controllers
                 context.SaveChanges();
             }
             */
-    
+
--- Services/BookService.cs
+++ Services/BookService.cs
@@ -22,9 +22,44 @@ namespace Bookish.Services
 
         public List<Book> GetBooks()
         {
-            List<Book> Books =_libraryContext.Books.ToList();
+            List<Book> Books = _libraryContext.Books.ToList();
             return Books;
         }
+
+        public Book GetASingleBook(int bookId)
+        {
+            var book = _libraryContext.Books
+                           .Where(b => b.Id == bookId)
+                           .FirstOrDefault();
+            return book;
+        }
+
+        public int CreateBook(Book model)
+        {
+            // var book = new Book()
+            // {
+            //     Title = model.Title,
+            //     Author = model.Author
+            // };
+            _libraryContext.Books.Add(model);
+            _libraryContext.SaveChanges();
+
+            return model.Id;
+        }
+
+        public void DeleteBook(Book model)
+        {
+
+            using (var context = new LibraryContext())
+            {
+                context.Books.Remove(model);
+                context.SaveChanges();
+            }
+            // _libraryContext.Books.Remove(model);
+            //  _libraryContext.SaveChanges();
+
+        }
+
 
     }
 }
